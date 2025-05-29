f9296bdf9780101848cac4a7643b497055a47e6f
HasanKazemi <avatarhasan78@gmail.com>
2025-05-29 21:37:18 +0330

    show summary in result page

--- app/searchResult/page.tsx
+++ app/searchResult/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import styles from "./searchResult.module.css"
+import Navbar from '@/components/Navbar'
 
 const SearchResult = () => {
     const searchParams = useSearchParams()
@@ -15,7 +16,7 @@ const SearchResult = () => {
       if (query) {
         const fetchRecipes = async () => {
           try {
-            const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}?query=${query}&number=20&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`);
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}?query=${query}&number=20&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&addRecipeInformation=true`);
             setRecipes(res.data.results);
           } catch (err) {
             console.error(err);
@@ -25,18 +26,17 @@ const SearchResult = () => {
       }
     }, [query]);
 
-  recipes.map(recipe=>{
-    console.log(recipe);
-  })
-
   return (
     <div>
+      <Navbar />
       <div className={styles.pageHeaderContainer}></div>
       <div className={styles.resultContainer}>
         {recipes.map(recipe=>(
             <div key={recipe.id} className={styles.resultCard}>
               <Image src={recipe.image} height={100} width={128} quality={95} alt='recipe' className={styles.thumbnail} />
               <h4 className={styles.title}>{recipe.title}</h4>
+              <p className={styles.summary}>{recipe.summary.replace(/<[^>]+>/g, '').slice(0,100)}...</p>
+              <button className={styles.moreBtn}>more details</button>
             </div>
           ))}
       </div>
--- types/types.ts
+++ types/types.ts
@@ -2,4 +2,5 @@ export interface Recipes {
     id: number;
     image: string;
     title: string;
+    summary: string;
 }
