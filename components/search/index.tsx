218afc81e6970f8325dd019f8b3e19afeeddc26f
HasanKazemi <avatarhasan78@gmail.com>
2025-05-29 14:36:07 +0330

    show result using user's search param

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,13 +1,23 @@
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './search.module.css'
 import { CiSearch } from "react-icons/ci";
 
 const Search = () => {
+  const [searchParam, setSearchParam] = useState<string>("")
+  const router = useRouter()
+  const handleRoute = ()=>{
+    router.push(`/searchResult?query=${searchParam}`)
+  }
   return (
     <div className={styles.searchContainer}>
         <h2 className={styles.searchTitle}>Search for recipes</h2>
         <div className={styles.searchBarContainer}>
-            <input type="text" className={styles.searchInput} placeholder="Search for recipes by ingredients, cuisine, or keywords..." />
-            <button className={styles.searchButton}><CiSearch fill='#112e01' /></button>
+            <input value={searchParam} onChange={(e)=>setSearchParam(e.target.value)} type="text" className={styles.searchInput} placeholder="Search for recipes by ingredients, cuisine, or keywords..." />
+            <button onClick={handleRoute} className={styles.searchButton}>
+              <CiSearch fill='#112e01' />
+            </button>
         </div>
     </div>
   )
