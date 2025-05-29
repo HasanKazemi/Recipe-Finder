6ccb8be185fba7002b635e400fcedd38b29586f3
HasanKazemi <avatarhasan78@gmail.com>
2025-05-29 08:29:53 +0330

    design search result page

---
+++ app/searchResult/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Recipes } from '@/types/types'
+import axios from 'axios'
+import Image from 'next/image'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import styles from "./searchResult.module.css"
+
+const SearchResult = () => {
+    const searchParams = useSearchParams()
+    const query = searchParams.get('query')
+    const [recipes, setRecipes] = useState<Recipes[] | []>([])
+
+    useEffect(() => {
+      if (query) {
+        const fetchRecipes = async () => {
+          try {
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}?query=${query}&number=20&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`);
+            setRecipes(res.data.results);
+          } catch (err) {
+            console.error(err);
+          }
+        };
+        fetchRecipes();
+      }
+    }, [query]);
+
+  recipes.map(recipe=>{
+    console.log(recipe);
+  })
+
+  return (
+    <div>
+      <div className={styles.pageHeaderContainer}></div>
+      <div className={styles.resultContainer}>
+        {recipes.map(recipe=>(
+            <div key={recipe.id} className={styles.resultCard}>
+              <Image src={recipe.image} height={100} width={128} quality={95} alt='recipe' className={styles.thumbnail} />
+              <h4 className={styles.title}>{recipe.title}</h4>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default SearchResult
---
+++ types/types.ts
@@ -0,0 +1,5 @@
+export interface Recipes {
+    id: number;
+    image: string;
+    title: string;
+}
