b12f43410280baf0700f97b063f833e1d92c736d
HasanKazemi <avatarhasan78@gmail.com>
2025-06-05 19:25:03 +0330

    improve result page UI

--- app/searchResult/page.tsx
+++ app/searchResult/page.tsx
@@ -57,9 +57,11 @@ const SearchResult = () => {
         {recipes.map(recipe=>(
             <div key={recipe.id} onClick={()=>router.push(`/details/${recipe.id}`)} className={styles.resultCard}>
               <Image src={recipe.image} height={100} width={128} quality={95} alt='recipe' className={styles.thumbnail} />
-              <h4 className={styles.title}>{recipe.title}</h4>
-              <p className={styles.summary}>{recipe.summary.replace(/<[^>]+>/g, '').slice(0,100)}...</p>
-              <button className={styles.moreBtn}>more details</button>
+              <div className={styles.cardBody}>
+                <h4 className={styles.title}>{recipe.title}</h4>
+                <p className={styles.summary}>{recipe.summary.replace(/<[^>]+>/g, '').slice(0,100)}...</p>
+                <button className={styles.moreBtn}>more details</button>
+              </div>
             </div>
           ))}
       </div>
