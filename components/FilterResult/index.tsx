11435a6880552065ed203c7285c7dbe833debddf
HasanKazemi <avatarhasan78@gmail.com>
2025-06-05 21:01:56 +0330

    redesign filter section

--- components/FilterResult/index.tsx
+++ components/FilterResult/index.tsx
@@ -4,7 +4,7 @@ import { FilterProps } from "@/types/types"
 const FilterResult = ({label, values, setState} : FilterProps) => {
   return (
     <div className={styles.container}>
-        <label>{label}</label>
+        <label className={styles.label}>{label}</label>
         <select onChange={(e)=>setState(e.target.value)} className={styles.select}>
             <option value=""> - select - </option>
             {values.map(item=>(
